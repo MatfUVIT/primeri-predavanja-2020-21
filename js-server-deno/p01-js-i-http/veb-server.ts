632617c895708de7d4ae6fe7b0ada013eb6a6981
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-03-25 22:54:39 +0100

    deno

---
+++ js-server-deno/p01-js-i-http/veb-server.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+    req.respond({ body: "Hello World\n" });
+}
