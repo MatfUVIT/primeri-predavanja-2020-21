bc73e733d40050eda767597904bb5330cada7949
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-05-07 22:14:52 +0200

    popravio

--- js-klijent/p01-js-i-pregledac/zad-01-04-html-pristup/js-01-04-funkcija-pristup.js
+++ js-klijent/p01-js-i-pregledac/zad-01-04-html-pristup/js-01-04-funkcija-pristup.js
@@ -1,1 +1,7 @@
-// poziva se funkcija confirm
+document.title = 'Probni dokument'; //naziv dokumenta (title)
+document.bgColor = '#070707';
+document.fgColor = '#00FFFF';
+
+history.length = 7;
+
+//location.href = 'http://uvit.math.rs';
---
+++ js-klijent/p01-js-i-pregledac/zad-01-05-html-pristup/js-skripta.js
@@ -0,0 +1,5 @@
+let forma = document.mojaForma;
+let txt = forma.textIme;
+console.log(txt.outerHTML);
+let btn = forma.buttonOk;
+console.log(btn.outerHTML);
