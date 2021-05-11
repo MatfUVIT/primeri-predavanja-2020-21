1ee43f12820ea39213243255ac36ad7acbc6dbb0
Vladimir Filipovic <vladofilipovic@hotmail.com>
2021-05-11 17:26:46 +0200

    поправио

--- js-klijent/p08-html-osobine-dogadjaji/zad08-03-kocka/kocka-component.js
+++ js-klijent/p08-html-osobine-dogadjaji/zad08-03-kocka/kocka-component.js
@@ -9,9 +9,7 @@ class Kocka extends HTMLElement {
     constructor() {
         // Always call super first in constructor
         super();
-
         const shadow = this.attachShadow({ mode: 'open' });
-
         const div = document.createElement('div');
         const style = document.createElement('style');
         shadow.appendChild(style);
